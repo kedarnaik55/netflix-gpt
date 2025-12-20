c78c43c162ebccc6ad53cc3d14ab614103bf96f5
Kedar Naik <kedarnaik55>
2025-12-21 03:29:14 +0530

    ep-01

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const AppLayout = () => {
+  return (
+    <div>hello</div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
